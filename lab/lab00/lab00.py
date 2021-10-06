32e3da0cef2b96589719d3c070fa53d2cf94c14b
baifu <810141192@qq.com>
2021-10-07 00:02:58 +0800

    作业

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,8 @@
+def twenty_eighteen():
+    """Come up with the most creative expression that evaluates to 2018,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_eighteen()
+    2018
+    """
+    return 1+1+3*4*5*6*7-6*5*4*3*2+8*9*10-9*8*7
